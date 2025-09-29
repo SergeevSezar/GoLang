006908f4c81cba70846948c3a8ada03a41304685
Sergeev Sergey <ssergeev@diasoft.ru>
2025-09-29 17:45:49 +0900

    "HW2 is completed"

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,45 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"unicode"
 )
 
-var ErrInvalidString = errors.New("invalid string")
+var ErrInvalidString = errors.New("некорректная строка")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	if len(s) == 0 {
+		return "", nil
+	}
+
+	var result []rune
+	runes := []rune(s)
+	i := 0
+
+	for i < len(runes) {
+
+		current := runes[i]
+		i++
+
+		if unicode.IsDigit(current) && len(result) == 0 {
+			return "", ErrInvalidString
+		}
+
+		if i < len(runes) && unicode.IsDigit(runes[i]) {
+			if i+1 < len(runes) && unicode.IsDigit(runes[i+1]) {
+				return "", ErrInvalidString
+			}
+
+			digit := runes[i]
+			count := int(digit - '0')
+			i++
+
+			for j := 0; j < count; j++ {
+				result = append(result, current)
+			}
+		} else {
+			result = append(result, current)
+		}
+	}
+
+	return string(result), nil
 }
