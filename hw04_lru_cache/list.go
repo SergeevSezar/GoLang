b32866b569770d05670695a331bc7270a3e020c8
Sergeev Sergey <ssergeev@diasoft.ru>
2025-12-19 19:28:04 +0900

    "HW4 add PushBack, Remove, MoveToFront function

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -53,3 +53,74 @@ func (l *list) PushFront(v interface{}) *ListItem {
 	l.len++
 	return newItem
 }
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	newItem := &ListItem{Value: v, Next: nil, Prev: nil}
+
+	if l.back == nil {
+		l.back = newItem
+		l.front = newItem
+	} else {
+		oldBack := l.back
+		newItem.Next = oldBack
+		oldBack.Prev = newItem
+		l.back = newItem
+	}
+	l.len++
+	return newItem
+}
+
+func (l *list) Remove(i *ListItem) {
+	prevItem := i.Prev
+	nextItem := i.Next
+
+	if prevItem != nil {
+		prevItem.Prev.Next = nextItem
+	} else {
+		l.front = nextItem
+	}
+
+	if nextItem != nil {
+		nextItem.Next.Prev = prevItem
+	} else {
+		l.back = prevItem
+	}
+
+	i.Next = nil
+	i.Prev = nil
+	l.len--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if l.front == i {
+		return
+	}
+
+	prevItem := i.Prev
+	nextItem := i.Next
+
+	if prevItem != nil {
+		prevItem.Next = nextItem
+	} else {
+		l.front = nextItem
+	}
+
+	if nextItem != nil {
+		nextItem.Prev = prevItem
+	} else {
+		l.back = prevItem
+	}
+
+	i.Prev = nil
+	i.Next = l.front
+
+	if l.front != nil {
+		l.front.Prev = i
+	}
+
+	l.front = i
+
+	if l.back == nil {
+		l.back = i
+	}
+}
